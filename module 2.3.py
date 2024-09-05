fa761f0c286d9126fde2a214c94c8bed417ca11f
Smetanka2002 <needfrig@gmail.com>
2024-09-05 17:10:27 +0300

    now code

---
+++ module 2.3.py
@@ -0,0 +1,27 @@
+
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+i = 0
+while i < len(my_list):
+    if my_list[i] > 0:
+        print(my_list[i])
+    elif my_list[i] < 0:
+        break
+    i = i + 1
+    fdsds
+
+
+
+
+    # if my_list[a] < 0:
+    #     break
+    # elif my_list[a] == 0:
+    #     a = a + 1
+    # else:
+    #
+    #     print(my_list[a])
+    #     a = a + 1
+
+
+
+
+
