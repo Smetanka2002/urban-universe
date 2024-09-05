eeb2d812963ffe7819d69d19c5c63ad7ec13e0b2
Smetanka2002 <needfrig@gmail.com>
2024-09-05 17:19:41 +0300

    iinit

--- module 1/zadanie 26.08.py
+++ module 1/zadanie 26.08.py
@@ -3,4 +3,5 @@ Home_work = 12
 time = 1.5
 direction = "Python"
 time_for_one_home_work = time / Home_work
+
 print("Курс: " + direction + ", всего задач: " + str(Home_work)+ ", затрачено часов: " + str(time), ", среднее время выполнения: " + str(time_for_one_home_work))
