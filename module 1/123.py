37a2035e95e659da814e646c14f45752a8565a3f
Smetanka2002 <needfrig@gmail.com>
2024-09-05 17:18:27 +0300

    init

---
+++ module 1/123.py
@@ -0,0 +1,16 @@
+
+
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+print(len(my_list))
+# a = 0
+# while len(range(my_list[a])) >= 0:
+#     number = []
+#     if my_list[a] > 0:
+#         print(my_list[a])
+#         a = a + 1
+#     else:
+#         a = a + 1
+#    # continue
+   # if my_list[0] < 0:
+   #     print("Больше чисел нет!")
+   # break
