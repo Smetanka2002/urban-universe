9ee74962229349c2fc8c48d3218231e52c44bd40
Smetanka2002 <needfrig@gmail.com>
2024-10-07 23:35:32 +0300

    iinit

---
+++ module_5/module_5_2.py
@@ -0,0 +1,30 @@
+#2023/10/30 00:00|Домашняя работа по уроку "Специальные методы классов"
+class House:
+    def  __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+    def go_to(self, new_floor):
+        if new_floor > self.number_of_floors or new_floor < 1:
+            print(f'{new_floor} - такого этажа не существует')
+        else:
+            for floor in range(new_floor):
+                print(floor + 1)
+    def __len__(self):
+        return self.number_of_floors
+    def __str__(self):
+        str_ = str(f"Название: {self.name}, кол-во этажей: {self.number_of_floors}")
+        return str_
+
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+
+# __str__
+print(h1)
+print(h2)
+
+# __len__
+print(len(h1))
+print(len(h2))
+
+
+
